01bd9c0edf57c7256d434ffedf91cc5597ff62bd
kakaocom <horiguchi.t@opst.co.jp>
2017-05-07 16:56:52 +0900

    Initial Commit

---
+++ RxTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RxTest
+//
+//  Created by Takao on 2017/05/07.
+//  Copyright © 2017年 TakaoHoriguchi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ RxTestTests/RxTestTests.swift
@@ -0,0 +1,36 @@
+//
+//  RxTestTests.swift
+//  RxTestTests
+//
+//  Created by Takao on 2017/05/07.
+//  Copyright © 2017年 TakaoHoriguchi. All rights reserved.
+//
+
+import XCTest
+@testable import RxTest
+
+class RxTestTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}

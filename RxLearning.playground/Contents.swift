08c6251af556ae432608c794400c460e84f7f8a4
kakaocom <horiguchi.t@opst.co.jp>
2017-06-05 00:01:58 +0900

    fix playground

--- RxLearning.playground/Contents.swift
+++ RxLearning.playground/Contents.swift
@@ -4,7 +4,9 @@ import UIKit
 import RxSwift
 import RxCocoa
 
-// RxSwift
+// Playgroundで非同期処理の実行をできるようにする
+import PlaygroundSupport
+PlaygroundPage.current.needsIndefiniteExecution = true
 
 // Observableの生成
 // let myFirstObservable: Observable<Int> = Observable.create でも可
@@ -19,6 +21,8 @@ let myFirstObservable = Observable<Int>.create { observer in
     return Disposables.create()
 }
 
+print("----------------------")
+
 // Observableの購読
 // subscribe: 引数のクロージャーはシーケンスから排出されたイベントを引数としており、新しいイベントが排出されるたびに呼び出される
 let subscription = myFirstObservable.subscribe { event in
@@ -32,3 +36,57 @@ let subscription = myFirstObservable.subscribe { event in
     }
 }
 subscription.dispose()
+
+print("----------------------")
+
+// オペレータの利用
+// map: Observerから排出される値を引数に取り、クロージャの中にその値を加工する処理を記述することができ、戻り値は新しいObservableになる
+// myFirstObservableに数値が流れてきたら、その数値を5倍するシーケンス
+let subscription1 = myFirstObservable.map{$0 * 5}.subscribe(onNext: { print("map: \($0)") })
+subscription1.dispose()
+
+print("----------------------")
+
+// RxCocoaを使って、Github APIにリクエストを送信する
+// 検索結果
+struct SearchResult {
+    let repos: [GithubRepository]
+    let totalCount: Int
+    init?(response:Any) {
+        guard let response = response as? [String:Any], let reposDictionaries = response["items"] as? [[String:Any]], let count = response["total_count"] as? Int else { return nil }
+        repos = reposDictionaries.flatMap{ GithubRepository(dictionary: $0) }
+        totalCount = count
+    }
+}
+
+// 検索結果のリポジトリ情報
+struct GithubRepository {
+    let name: String
+    let starCount:Int
+    init(dictionary:[String:Any]) {
+        name = dictionary["full_name"] as! String
+        starCount = dictionary["stargazers_count"] as! Int
+    }
+}
+
+// 文字列を受け取って、Observableシーケンスを返す
+///
+///
+/// - Parameter keyword: 検索キーワード
+/// - Returns: SerchResult型を排出するObservableシーケンス
+// このObservableシーケンスを購読することで、非同期に実行されるリポジトリ検索の結果を観測したり操作したりすることが可能になる
+func searchRepos(keyword: String) -> Observable<SearchResult?> {
+    let endPoint = "https://api.github.com"
+    let path = "/search/repositories"
+    let query = "?q=\(keyword)"
+    let url = URL(string: endPoint + path + query)!
+    let request = URLRequest(url: url)
+    
+    // rx.json(request:): 引数のURLRequestを実行し、レスポンスのJSONをデシリアライズし、結果をObservableシーケンスに排出してくれる
+    // mapオペレータでAPIのレスポンス結果をAny型→SearchResult型に変換して新たにObservable<SearchResult>を生成する
+    return URLSession.shared.rx.json(request: request).map{ SearchResult(response: $0)}
+}
+
+// SearchReposの戻り値であるObservableをsubscribeする
+// subscribeした瞬間にAPI通信の非同期処理が実行され、処理が成功すればSearchResultの値がonNextの値としてシーケンスに排出される
+let subscription2 = searchRepos(keyword: "RxSwift").subscribe(onNext: {print($0)})
